0a46717fb88fbd2054135665444f42c2cadee4ab
Arvin M <rhenmedina20@gmail.com>
2023-11-25 07:04:47 +0800

    chore: i hope this works

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        proxy: {
+            "/api/rest": {
+                target: "https:/lasting-parrot-91.hasura.app/",
+                changeOrigin: true,
+                secure: true,
+            },
+        },
+    },
 })
