273202368f40e5982cae64bec6da7b6d25b989f9
Arvin M <rhenmedina20@gmail.com>
2023-11-25 05:54:44 +0800

    chore: removed react buddy and changed env .

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: import.meta.env.VITE_APP_BASE_URL,
+    baseURL: import.meta.env.VITE_APP_HASURA_URL,
     headers: {
         'X-Hasura-Admin-Secret': import.meta.env.VITE_APP_HASURA_SECRET_KEY,
     },
