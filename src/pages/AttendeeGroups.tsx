c25e21811dab0098d5cdfc5ab481682865c28313
Arvin M <rhenmedina20@gmail.com>
2023-11-27 11:23:07 +0800

    chore: changed to group score

--- src/pages/AttendeeGroups.tsx
+++ src/pages/AttendeeGroups.tsx
@@ -5,7 +5,7 @@ function AttendeeGroups() {
     return (
         <div className="flex-1 justify-center p-5 m-auto items-center">
             <div className="justify-center m-auto">
-                <div className="text-center w-full text-4xl md:text-9xl font-custom pb-3">Attendee's Groups</div>
+                <div className="text-center w-full text-4xl md:text-9xl font-custom pb-3">Group Score</div>
                 <div className="flex justify-center text-center">
                     <GroupedTable/>
                 </div>
